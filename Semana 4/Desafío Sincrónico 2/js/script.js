b85651c37ac3470693be84e38745791be59f0798
Eva <eva_elizabeth_frola@hotmail.com>
2025-03-04 17:06:38 -0300

    Solucion del desafio

--- Semana 4/Desafío Sincrónico 2/js/script.js
+++ Semana 4/Desafío Sincrónico 2/js/script.js
@@ -5,7 +5,23 @@ const DATA = [
   { name: "Gian", lastname: "Laic" },
   { name: "Fernando", lastname: "Saavedra" },
 ];
+let container = document.getElementById("container");
 
-// Escribe el código necesario aquí
+function showNames (array) {
 
-//
+  let contentHTML = "";
+
+  for (let i = 0; i < array.length; i++) {
+  
+    let nameList = array[i];
+    contentHTML += `
+      <p> ${nameList.name} ${nameList.lastname}</p>
+    `
+  };
+
+  container.innerHTML = contentHTML;
+}
+
+document.addEventListener("DOMContentLoaded", ()=> {
+  showNames(DATA);
+})
