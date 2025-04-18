a2c92352af08cc09f92dea5eac2bae7d49e7bf51
Eva <eva_elizabeth_frola@hotmail.com>
2025-04-18 20:50:14 -0300

    Proyecto semana 7 termiando

--- Semana 7/tallerRegistro/js/validacion.js
+++ Semana 7/tallerRegistro/js/validacion.js
@@ -1,7 +1,42 @@
+//VARIABLES
+const nombreInput = document.getElementById("nombre");
+const apellidoInput = document.getElementById("apellido");
+const emailInput = document.getElementById("email");
+const password1Input = document.getElementById("password1");
+const password2Input = document.getElementById("password2");
+const terminos = document.getElementById("terminos");
+const form =  document.getElementById("regBtn");
+
+//ALERTA DE FORMULARIO EXITOSO
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
+    //TEMPORIZADOR PARA REMOVER ALERTA
+    setTimeout(() => {
+    document.getElementById("alert-success").classList.remove("show");    
+    }, 2000);
 }
 
+//ALERTA DE ERRORES
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
+    //TEMPORIZADOR PARA REMOVER ALERTA
+    setTimeout(() => {
+    document.getElementById("alert-danger").classList.remove("show");    
+    }, 2000);
+    
 }
+
+//VALIDACIÓN DE FORMULARIO
+form.addEventListener("click", ()=> {
+    if ((nombreInput.value.length) === 0 || 
+        (apellidoInput.value.length) === 0 ||
+        (emailInput.value.length) === 0 ||
+        (password1Input.value.length) < 6 ||
+        (password2Input.value !== password1Input.value) || 
+        (!terminos.checked)
+    ) {
+        showAlertError()
+    } else {
+        showAlertSuccess()
+    }
+});
