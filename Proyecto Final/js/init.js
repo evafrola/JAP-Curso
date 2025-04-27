169f42fbab5e1e78c82bb83efaacf63746d894d1
Eva <eva_elizabeth_frola@hotmail.com>
2025-04-27 18:09:52 -0300

    Creando contenido responsive

--- Proyecto Final/js/init.js
+++ Proyecto Final/js/init.js
@@ -6,6 +6,8 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/prod
 const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
+const navbar_movil = getElementById("navbar_movil");
+const navbar_pc = getElementById("navbar_pc");
 
 let showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
@@ -39,3 +41,23 @@ let getJSONData = function(url){
         return result;
     });
 }
+
+ //Función que se ejecuta teniendo en cuenta el tamaño del documento del usuario
+ function changeDocument () {
+  if(document.documentElement.clientWidth <= 640){
+      //Se les agrega una clase que quita la visualización del elemento en la pagina
+      navbar_pc.classList.add("hidden");
+  }}
+
+  //Función para remover la clase invisible y devuelve el contenido al documento, se elimina el menu desplegable de la barra de navegación
+  function removeChange() {
+    if (document.documentElement.clientWidth > 640) {
+        navbar_pc.classList.remove("invisibility");
+    }
+  }
+
+  //Evento que detecta un cambio en el tamaño de ventana y ejecuta las funciones
+  window.addEventListener("resize", () => {
+    changeDocument()
+    removeChange()
+  })
