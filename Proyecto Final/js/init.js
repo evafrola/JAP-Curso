63e608c7c92798c30eb2a8fb13b7fda123a00128
Eva <eva_elizabeth_frola@hotmail.com>
2025-05-27 17:39:13 -0300

    Modificacion de boton de inicio para movil

--- Proyecto Final/js/init.js
+++ Proyecto Final/js/init.js
@@ -6,7 +6,6 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/prod
 const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
-const navbar_movil = getElementById("navbar_movil");
 const navbar_pc = getElementById("navbar_pc");
 
 let showSpinner = function(){
